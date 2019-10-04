798773d7002f44144d0e11e14e645670116e859a
Adrian <adrianmejias@gmail.com>
2019-10-03 22:26:25 -0500

    chrome audit fixes

--- src/js/components/App.vue
+++ src/js/components/App.vue
@@ -2,7 +2,7 @@
     <div v-cloak>
         <header class="container mx-auto my-6 flex flex-row flex-wrap justify-between">
             <div class="flex flex-row flex-wrap items-center">
-                <a href="https://hacktoberfest.digitalocean.com" title="Hacktoberfest 2019">
+                <a href="https://hacktoberfest.digitalocean.com" title="Hacktoberfest 2019" target="_blank" rel="noreferrer">
                     <img class="mx-auto" src="/images/header.svg" alt="Hacktoberfest 2019" width="400px">
                 </a>
                 <h1 class="text-5xl font-bold text-primary uppercase mx-auto my-4 md:my-0 text-center">
@@ -71,14 +71,16 @@
         <footer class="bg-primary w-full mt-8">
             <div class="container mx-auto flex flex-row items-center justify-between p-8">
                 <div class="flex flex-row items-center justify-center">
-                    <p class="text-amber">Unofficial site by <a class="text-secondary" href="http://duncan.mcclean.co.uk">Duncan McClean</a> and <a class="text-secondary" href="https://github.com/damcclean/hacktoberfest-finder/blob/master/CONTRIBUTORS.md">contributors</a>. Contribute on <a class="text-secondary" href="https://github.com/damcclean/hacktoberfest-finder" target="_blank">GitHub!</a></p>
+                    <p class="text-amber">
+                        Unofficial site by <a class="text-secondary" href="http://duncan.mcclean.co.uk" target="_blank" title="Duncan McClean" rel="noreferrer">Duncan McClean</a> and <a class="text-secondary" href="https://github.com/damcclean/hacktoberfest-finder/blob/master/CONTRIBUTORS.md" target="_blank" title="contributors" rel="noreferrer">contributors</a>. Contribute on <a class="text-secondary" href="https://github.com/damcclean/hacktoberfest-finder" target="_blank" title="GitHub!" rel="noreferrer">GitHub!</a>
+                    </p>
                 </div>
                 <div class="flex flex-row items-center justify-center">
-                    <a class="mx-2 w-6 h-6" href="https://www.digitalocean.com">
-                        <img class="w-6 h-6" src="/images/digital-ocean.png">
+                    <a class="mx-2 w-6 h-6" href="https://www.digitalocean.com" title="DigitalOcean" target="_blank" rel="noreferrer">
+                        <img class="w-6 h-6" src="/images/digital-ocean.png" alt="DigitalOcean">
                     </a>
-                    <a class="mx-2 w-6 h-6" href="https://dev.to">
-                        <img class="w-6 h-6" src="/images/dev.svg">
+                    <a class="mx-2 w-6 h-6" href="https://dev.to" title="DEV" target="_blank" rel="noreferrer">
+                        <img class="w-6 h-6" src="/images/dev.svg" alt="DEV">
                     </a>
                 </div>
             </div>
