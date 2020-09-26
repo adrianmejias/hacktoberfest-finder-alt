31fd7dc18710711cc1370fad64d6055c248c39db
Adrian Mejias <adrianmejias@gmail.com>
2020-09-26 16:52:17 -0500

    2020 update

--- src/js/components/AppMainModal.vue
+++ src/js/components/AppMainModal.vue
@@ -1,50 +1,99 @@
 <template>
-    <section class="grid-item-container fixed top-0 bottom-0 left-0 right-0 max-w-full h-full" @click="closeModal" v-cloak>
-        <a :href="currentResult.html_url" target="_blank" class="bg-white hover:bg-pinkish rounded-lg p-2 text-black flex flex-col justify-center items-center overflow-hidden overflow-y-auto shadow-lg grid-item max-w-md mx-auto" :title="currentResult.title" @click.prevent.stop="openWindow">
-            <div class="flex flex-wrap flex-row w-full items-center justify-center mb-2">
-                <span v-for="label in currentResult.labels" class="text-center rounded-sm text-xs px-2 py-1 m-1 shadow-md" :style="{ backgroundColor: `#${label.color}` }" :class="{ 'font-bold': labels.includes(label.name.toLowerCase()) }" v-text="label.name.toLowerCase()"></span>
-            </div>
-            <h2 class="text-center text-xl mb-4 font-bold" v-text="currentResult.title"></h2>
-            <h3 class="text-center text-blue text-sm mb-4 break-words" v-text="`${currentResult.user.login}/${currentResult.repo_title}`"></h3>
-            <div class="text-left border-gray border-solid border-l-4 pl-2 text-sm mb-4 overflow-hidden overflow-y-scroll max-w-sm whitespace-pre-wrap h-full" v-text="currentResult.body" style="max-height: 14rem;"></div>
-            <div class="text-center text-xs mb-4" :class="{ 'text-amber': !currentResult.comments }" v-if="!noReplyOnly" v-text="currentResult.comments > 0 ? `Replies: ${currentResult.comments}` : 'Gimme Gimme'"></div>
-            <div class="text-left text-xs mb-4" v-text="`Opend by: ${currentResult.user.login}`"></div>
-            <div class="text-center text-xs mb-4">
-                <time :datetime="currentResult.unformatted_date" v-text="`Last updated: ${currentResult.formatted_date}`"></time>
-            </div>
-        </a>
-    </section>
+  <section
+    class="fixed top-0 bottom-0 left-0 right-0 h-full max-w-full grid-item-container"
+    @click="closeModal"
+    v-cloak
+  >
+    <a
+      :href="currentResult.html_url"
+      target="_blank"
+      class="flex flex-col items-center justify-center max-w-md p-2 mx-auto overflow-hidden overflow-y-auto text-black bg-white rounded-lg shadow-lg hover:bg-pinkish grid-item"
+      :title="currentResult.title"
+      @click.prevent.stop="openWindow"
+    >
+      <div
+        class="flex flex-row flex-wrap items-center justify-center w-full mb-2"
+      >
+        <span
+          v-for="label in currentResult.labels"
+          :key="label.name"
+          class="px-2 py-1 m-1 text-xs text-center rounded-sm shadow-md"
+          :style="{ backgroundColor: `#${label.color}` }"
+          :class="{ 'font-bold': labels.includes(label.name.toLowerCase()) }"
+          v-text="label.name.toLowerCase()"
+        ></span>
+      </div>
+      <h2
+        class="mb-4 text-xl font-bold text-center"
+        v-text="currentResult.title"
+      ></h2>
+      <h3
+        class="mb-4 text-sm text-center break-words text-blue"
+        v-text="`${currentResult.user.login}/${currentResult.repo_title}`"
+      ></h3>
+      <div
+        class="h-full max-w-sm pl-2 mb-4 overflow-hidden overflow-y-scroll text-sm text-left whitespace-pre-wrap border-l-4 border-solid border-gray"
+        v-text="currentResult.body"
+        style="max-height: 14rem"
+      ></div>
+      <div
+        class="mb-4 text-xs text-center"
+        :class="{ 'text-amber': !currentResult.comments }"
+        v-if="!noReplyOnly"
+        v-text="
+          currentResult.comments > 0
+            ? `Replies: ${currentResult.comments}`
+            : 'Gimme Gimme'
+        "
+      ></div>
+      <div
+        class="mb-4 text-xs text-left"
+        v-text="`Opend by: ${currentResult.user.login}`"
+      ></div>
+      <div class="mb-4 text-xs text-center">
+        <time
+          :datetime="currentResult.unformatted_date"
+          v-text="`Last updated: ${currentResult.formatted_date}`"
+        ></time>
+      </div>
+    </a>
+  </section>
 </template>
 
 <script>
-    export default {
-        props: ['labels', 'noReplyOnly', 'currentResult'],
+export default {
+  props: ["labels", "noReplyOnly", "currentResult"],
 
-        mounted() {
-            (() => {
-                document.addEventListener('keydown', this.escapeModal, false);
-            })();
-        },
+  mounted() {
+    (() => {
+      document.addEventListener("keydown", this.escapeModal, false);
+    })();
+  },
 
-        beforeDestroy() {
-            document.removeEventListener('keydown', this.escapeModal, false);
-        },
+  beforeDestroy() {
+    document.removeEventListener("keydown", this.escapeModal, false);
+  },
 
-        methods: {
-            escapeModal(event) {
-                if ((event.key === 'Escape' || event.key === 'Esc' || event.keyCode === 27) && (event.target.nodeName.toLowerCase() === 'body')) {
-                    event.preventDefault();
-                    this.closeModal();
-                }
-            },
+  methods: {
+    escapeModal(event) {
+      if (
+        (event.key === "Escape" ||
+          event.key === "Esc" ||
+          event.keyCode === 27) &&
+        event.target.nodeName.toLowerCase() === "body"
+      ) {
+        event.preventDefault();
+        this.closeModal();
+      }
+    },
 
-            closeModal() {
-                Bus.$emit('closeModal');
-            },
+    closeModal() {
+      Bus.$emit("closeModal");
+    },
 
-            openWindow() {
-                window.open(this.currentResult.html_url, 'hofWindow');
-            }
-        }
-    }
+    openWindow() {
+      window.open(this.currentResult.html_url, "hofWindow");
+    },
+  },
+};
 </script>
--- webpack.mix.js
+++ webpack.mix.js
@@ -8,79 +8,78 @@ require("laravel-mix-postcss-config");
 
 console.info(`Generating assets for ${process.env.NODE_ENV} environment.`);
 
-mix
-  .setPublicPath("public")
-  .js("src/js/app.js", "public/js")
-  .postCss("src/css/app.css", "public/css")
-  .options({
-    processCssUrls: false,
-    postCss: [tailwindcss]
-  })
-  .postCssConfig({
-    plugins: [postCSSNested()]
-  })
-  .purgeCss({
-    enabled: mix.inProduction(),
-    globs: [path.join(__dirname, "public/index.html")],
-    extensions: ["html", "js"],
-    folders: ["src", "public"]
-  })
-  .webpackConfig({
-    plugins: [
-      new SWPrecacheWebpackPlugin({
-        cacheId: "hacktoberfest",
-        dontCacheBustUrlsMatching: /\.\w{8}\./,
-        filename: "service-worker.js",
-        navigateFallback: "/index.html",
-        staticFileGlobs: [
-          "public/css/**.*",
-          "public/favicons/**.*",
-          "public/images/**.*",
-          "public/js/**.*",
-          "public/index.html"
+mix.setPublicPath("public")
+    .js("src/js/app.js", "public/js")
+    .postCss("src/css/app.css", "public/css")
+    .options({
+        processCssUrls: false,
+        postCss: [tailwindcss],
+    })
+    .postCssConfig({
+        plugins: [postCSSNested()],
+    })
+    .purgeCss({
+        enabled: mix.inProduction(),
+        globs: [path.join(__dirname, "public/index.html")],
+        extensions: ["html", "js"],
+        folders: ["src", "public"],
+    })
+    .webpackConfig({
+        plugins: [
+            new SWPrecacheWebpackPlugin({
+                cacheId: "hacktoberfest",
+                dontCacheBustUrlsMatching: /\.\w{8}\./,
+                filename: "service-worker.js",
+                navigateFallback: "/index.html",
+                staticFileGlobs: [
+                    "public/css/**.*",
+                    "public/favicons/**.*",
+                    "public/images/**.*",
+                    "public/js/**.*",
+                    "public/index.html",
+                ],
+                mergeStaticsConfig: true,
+                minify: mix.inProduction(),
+                stripPrefix: "public/",
+                handleFetch: true,
+                dynamicUrlToDependencies: {
+                    "/": ["public/index.html"],
+                },
+                staticFileGlobsIgnorePatterns: [
+                    /\.map$/,
+                    /mix\-manifest\.json$/,
+                    /manifest\.json$/,
+                    /browserconfig\.xml$/,
+                    /service\-worker\.js$/,
+                    /robots\.txt$/,
+                ],
+                runtimeCaching: [
+                    {
+                        urlPattern: /^https:\/\/fonts\.(googleapis|gstatic)\.com\/css/,
+                        handler: "cacheFirst",
+                    },
+                    {
+                        urlPattern: /^https:\/\/([a-z0-9]+)\.cloudfront\.net\//,
+                        handler: "cacheFirst",
+                    },
+                    {
+                        urlPattern: /^https:\/\/api\.github\.com\/search\/issues/,
+                        handler: "networkFirst",
+                    },
+                    {
+                        urlPattern: /\/(images|favicons)\/(\w+)\.(png|jpg|gif|jpeg|jpe|bmp|svg|ico)$/,
+                        handler: "cacheFirst",
+                    },
+                ],
+            }),
         ],
-        mergeStaticsConfig: true,
-        minify: mix.inProduction(),
-        stripPrefix: "public/",
-        handleFetch: true,
-        dynamicUrlToDependencies: {
-          "/": ["public/index.html"]
-        },
-        staticFileGlobsIgnorePatterns: [
-          /\.map$/,
-          /mix\-manifest\.json$/,
-          /manifest\.json$/,
-          /browserconfig\.xml$/,
-          /service\-worker\.js$/,
-          /robots\.txt$/
-        ],
-        runtimeCaching: [
-          {
-            urlPattern: /^https:\/\/fonts\.(googleapis|gstatic)\.com\/css/,
-            handler: "cacheFirst"
-          },
-          {
-            urlPattern: /^https:\/\/([a-z0-9]+)\.cloudfront\.net\//,
-            handler: "cacheFirst"
-          },
-          {
-            urlPattern: /^https:\/\/api\.github\.com\/search\/issues/,
-            handler: "networkFirst"
-          },
-          {
-            urlPattern: /\/(images|favicons)\/(\w+)\.(png|jpg|gif|jpeg|jpe|bmp|svg|ico)$/,
-            handler: "cacheFirst"
-          }
-        ]
-      })
-    ]
-  })
-  .extract(["vue"]);
+    })
+    .extract(["vue"]);
 
 if (mix.inProduction()) {
-  mix.version().disableNotifications();
+    mix.version().disableNotifications();
 } else {
-  mix.sourceMaps().webpackConfig({
-    devtool: "inline-source-map"
-  });
+    mix.sourceMaps().webpackConfig({
+        devtool: "inline-source-map",
+    });
 }
